0a8276dd010e2b2c60c5800dc44ab4b0b51ecf51
alexdaffern <alex.daffern@gmail.com>
2022-02-11 15:18:46 +0200

    Update Error.js

--- src/components/Error.js
+++ src/components/Error.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 
 const Error = ({ message }) => (
-    <div className={` alert position-absolute`} role="alert">
+    <div className="center" role="alert">
         {message}
     </div>
 );
